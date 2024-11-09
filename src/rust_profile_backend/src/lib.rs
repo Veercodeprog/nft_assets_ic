2329835712bbcab83cc8fd6f012fa60b4d146403
veercodeprog <veerpratap945050@gmail.com>
2024-11-10 03:03:41 +0530

    updated

--- src/rust_profile_backend/src/lib.rs
+++ src/rust_profile_backend/src/lib.rs
@@ -1,4 +1,72 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Deserialize, Principal};
+use ic_cdk::{query, update};
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+
+type IdStore = BTreeMap<String, Principal>;
+type ProfileStore = BTreeMap<Principal, Profile>;
+
+#[derive(Clone, Debug, Default, CandidType, Deserialize)]
+struct Profile {
+    pub name: String,
+    pub description: String,
+    pub keywords: Vec<String>,
+}
+
+thread_local! {
+    static PROFILE_STORE: RefCell<ProfileStore> = RefCell::default();
+    static ID_STORE: RefCell<IdStore> = RefCell::default();
+}
+
+#[query(name = "getSelf")]
+fn get_self() -> Profile {
+    let id = ic_cdk::api::caller();
+    PROFILE_STORE.with(|profile_store| profile_store.borrow().get(&id).cloned().unwrap_or_default())
+}
+
+#[query]
+fn get(name: String) -> Profile {
+    ID_STORE.with(|id_store| {
+        PROFILE_STORE.with(|profile_store| {
+            id_store
+                .borrow()
+                .get(&name)
+                .and_then(|id| profile_store.borrow().get(id).cloned())
+                .unwrap_or_default()
+        })
+    })
+}
+
+#[update]
+fn update(profile: Profile) {
+    let principal_id = ic_cdk::api::caller();
+    ID_STORE.with(|id_store| {
+        id_store
+            .borrow_mut()
+            .insert(profile.name.clone(), principal_id);
+    });
+    PROFILE_STORE.with(|profile_store| {
+        profile_store.borrow_mut().insert(principal_id, profile);
+    });
+}
+
+#[query]
+fn search(search_parameter: String) -> Option<Profile> {
+    PROFILE_STORE.with(|profile_store| {
+        let profiles = profile_store.borrow();
+
+        for (_, profile) in profiles.iter() {
+            if profile.name.contains(&search_parameter)
+                || profile.description.contains(&search_parameter)
+                || profile
+                    .keywords
+                    .iter()
+                    .any(|keyword| keyword.contains(&search_parameter))
+            {
+                return Some(profile.clone());
+            }
+        }
+
+        None
+    })
 }
